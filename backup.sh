aa828ff21ed1d97410ea03c96087b16cf2aca8fa
sebastienserre <sebastien@thivinfo.com>
2015-12-29 09:46:22 +0100

    initial commit

---
+++ backup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# @author: Sébastien SERRE
+# @mail: sebastien@thivinfo.com
+# @url: www.sebastien-serre.fr
+# License: GPL
+#
+# Backup directory constant
+DST=/path/to/files/backup/storage
+DSTDB=/path/to/files/backup/storage
+DATE=`date +%d-%m-%Y`
+ADRESS=yourmail@domain.tld
+
+## Delete more than X days backup
+#
+echo "Start Cleaning : "`date "+%d-%m-%Y %T"` >> $DST/log
+find $DST -type f -mtime +X | xargs rm -f # replace X by number of days you want to keep
+find $DSTDB -type f -mtime +X |xargs rm -f # replace X by number of days you want to keep
+echo "Stop Cleaning : "`date "+%d-%m-%Y %T"` >> $DST/log
+
+## going to backup folders
+#
+echo "Start backup db : "`date "+%d-%m-%Y %T"` >> $DST/log
+cd $DSTDB
+
+for i in name_of_your_db_separated_by_a space; do
+
+## Sauvegarde des bases de donnees en fichiers .sql
+mysqldump --user=XXX --password=XXXXX $i > ${i}_`date +%D | sed 's;/;-;g'`.sql   #replace XXXX by useer and passord to access to mysql
+
+## Compress to tar.bz2 (best rate)
+tar jcf ${i}_`date "+%d-%m-%Y" | sed 's;/;-;g'`.sql.tar.bz2 ${i}_`date "+%d-%m-%Y" | sed 's;/;-;g'`.sql
+
+## delete uncompressed backup
+rm ${i}_`date "+%d-%m-%Y" | sed 's;/;-;g'`.sql
+done
+
+echo "End backup db : "`date "+%d-%m-%Y %T"` >> $DST/log
+echo "Start Copy : "`date "+%d-%m-%Y %T"` >> $DST/log
+cd $DST
+if [ ! -d "temp/" ]; then
+	mkdir temp/;
+fi
+
+#files backup; add path separated by a space
+for i in path/to/file/to/backup path/to/file/to/backup;
+			do
+				cd $i;
+    			folder=$(basename `pwd`);
+    			rsync -rvu --delete-excluded --exclude-from= path/to/exclude.txt $i $DST/temp/
+				tar -zcf $DST/files/$folder-$DATE.tar.gz $DST/temp/$folder
+			done
+
+			echo "End Copy : "`date "+%d-%m-%Y %T"` >> $DST/log
+
+			#start rsync to a remote host
+			echo "Start rsync : "`date "+%d-%m-%Y %T"` >> $DST/log
+			rsync -e ssh -avz --exclude="temp" --delete-after ~/main/path/backup/ user@fdomain:~/path/to/store/backup/in/remote/host
+			echo "Stop rsync : "`date "+%d-%m-%Y %T"` >> $DST/log
+			echo "Backup terminé le "`date "+%d-%m-%Y %T"` $LOG | mail -s "backup du $DATE" $ADRESS
+			echo "Mail envoyé : "`date "+%d-%m-%Y %T"` >> $DST/log
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+INTRODUCTION
+============
+
+Bash-Backup will automate your Backup (file+db) if you add a cron task with this script.
+
+INSTALLATION
+------------
+Simply upload this backup.sh file to a directory in your main host and adapt it to make it working!
+
+
+
+DONATION
+--------
+if this script is helping you, please visit my donation page, it willencourage me to add features and correct bugs.
+http://sebastien-serre.fr/dons/
